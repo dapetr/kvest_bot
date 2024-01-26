a54d95c84ab586e828f9f1c2f1918c14998b58a7
Your Name <miss.petrovadaria@yandex.ru>
2024-01-26 22:39:48 +0300

    new

--- bottt.py
+++ bottt.py
@@ -1,25 +1,49 @@
 import telebot
 from telebot.types import ReplyKeyboardMarkup, KeyboardButton
 from data_users import load_data, save_data
+from locations import location_data
 
-token = 'нет'
+token = '6457885818:AAHVcYEAP10xizU2FAHaVRNcub1ViPwjrq0'
 bot = telebot.TeleBot(token)
 
-data_path = "users.json"
-user_data = load_data(data_path)
+data = "users.json"
+user_data = load_data(data)
 
 
-def make_keyboard(buttons):
-    keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
-    for ell in buttons:
-        keyboard.add(KeyboardButton(ell))
+def make_keyboard(options):
+    keyboard = ReplyKeyboardMarkup(one_time_keyboard=True, resize_keyboard=True)
+    for i in options.keys():
+        keyboard.add(KeyboardButton(i))
 
     return keyboard
 
 
 @bot.message_handler(content_types=['text'], commands=['start'])
 def start_message(message):
-    bot.send_message(message.chat.id, "Привет!")
+    global user_data
+    user_id = message.chat.id
+    bot.send_message(user_id, "Привет!")
+
+    if user_id in user_data:
+        user_data[user_id]["location"] = "start"
+    else:
+        user_data[user_id] = {"location": "start"}
+
+    location = user_data[user_id]["location"]
+    send_location(message.chat.id, location)
+    save_data(user_data, data)
+
+
+def send_location(chat_id, location):
+    cur_location = location_data[location]
+    options = cur_location["options"]
+    picture_patch = cur_location["picture"]
+    description = cur_location["description"]
+
+    keyboard = make_keyboard(options)
+
+    with open(picture_patch, "rb") as photo:
+        bot.send_photo(chat_id, photo, caption=description, reply_markup=keyboard)
 
 
 @bot.message_handler(content_types=['text'], commands=['help'])
--- data_users.py
+++ data_users.py
@@ -1,15 +1,17 @@
 import json
+import os
 
 
-def load_data(data_path):
-    try:
-        with open(data_path, "r", encoding="UTF-8") as f:
-            data = json.load(f)
-    except FileNotFoundError:
-        data = {}
+def load_data(data):
+    if os.path.exists(data):
+        try:
+            with open(data, "r", encoding="UTF-8") as f:
+                data = json.load(f)
+        except FileNotFoundError:
+            data = {}
     return data
 
 
-def save_data(user_data, data_path):
-    with open(data_path, "w", encoding="UTF-8") as f:
+def save_data(user_data, data):
+    with open(data, "w", encoding="UTF-8") as f:
         json.dump(user_data, f)
