3f0ef9cade26a985f3c989b4cf3013336f5f2c1f
Your Name <miss.petrovadaria@yandex.ru>
2024-01-28 20:22:26 +0300

    done

--- bottt.py
+++ bottt.py
@@ -1,5 +1,5 @@
 import telebot
-from telebot.types import ReplyKeyboardMarkup, KeyboardButton
+from telebot.types import ReplyKeyboardMarkup, KeyboardButton, Message
 from data_users import load_data, save_data
 from locations import location_data
 
@@ -23,9 +23,13 @@ def start_message(message):
     global user_data
     user_id = message.chat.id
     bot.send_message(user_id, "Рекомендую включить эту песню для особой атмосферы)")
-    audio = open(r'Bernth_-_Farewell.mp3', 'rb')
-    bot.send_audio(message.chat.id, audio)
-    audio.close()
+
+    try:
+        audio = open(r'Bernth_-_Farewell.mp3', 'rb')
+        bot.send_audio(message.chat.id, audio)
+        audio.close()
+    except:
+        bot.send_message(user_id, "Извините, не получилось отправить песню(")
 
     if user_id in user_data:
         user_data[user_id]["location"] = "start"
@@ -33,15 +37,17 @@ def start_message(message):
         user_data[user_id] = {"location": "start"}
 
     location = user_data[user_id]["location"]
-    send_location(message.chat.id, location, message)
     save_data(user_data, data)
+    send_location(message.chat.id, location)
 
 
-def send_location(chat_id, location, message):
+def send_location(chat_id, location):
     cur_location = location_data[location]
     options = cur_location["options"]
     picture_patch = cur_location["picture"]
     description = cur_location["description"]
+
+    print(f"Sending location: {location}")
 
     if "final" in cur_location:
         with open(picture_patch, "rb") as photo:
@@ -50,25 +56,26 @@ def send_location(chat_id, location, message):
         with open(picture_patch, "rb") as photo:
             bot.send_photo(chat_id, photo, caption=description, reply_markup=make_keyboard(options))
 
-        @bot.message_handler(func=lambda m: True, content_types=['text'])
-        def handle_text_answer(message):
-            handle_answer(chat_id, message)
+
+@bot.message_handler(func=lambda m: True, content_types=['text'])
+def handle_text_answer(message: Message):
+    user_id = message.chat.id
+    print(message.text)
+    handle_answer(user_id, message.text)
 
 
-def handle_answer(user_id, message):
+def handle_answer(user_id, user_input):
     global user_data
-    user_input = message.text.lower()
 
     current_location = user_data[user_id]["location"]
-    cur_location_data = location_data[current_location]
-    options = cur_location_data["options"]
+    options = location_data[current_location]["options"]
 
     if user_input in options:
         next_location = options[user_input]
         user_data[user_id]["location"] = next_location
-
-        send_location(user_id, next_location, message)
+        print(f"New location set to: {next_location}")
         save_data(user_data, data)
+        send_location(user_id, next_location)
 
 
 @bot.message_handler(content_types=['text'], commands=['help'])
